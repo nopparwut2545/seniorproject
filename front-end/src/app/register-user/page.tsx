42fe45cabe0632cfda271f6e3b1100253d995317
Chalumpu Atjarit <mhoomind_@hotmail.com>
2024-01-07 01:29:28 +0700

    Upload: UI - register - user (Responsive)

--- front-end/src/app/register-user/page.tsx
+++ front-end/src/app/register-user/page.tsx
@@ -1,8 +1,12 @@
 'use client'
+import RootLayout from "../layout";
+import logo from '../../../assets/Logo.png';
+import Image from "next/image";
 import React, { FormEvent, useState } from "react";
 import styles from "../../../styles/Register.module.css";
 import axios from "axios";
 import { useRouter } from 'next/navigation'
+import Link from "next/link";
 type Props = {};
 
 export default function RegisterUserpage({ }: Props) {
@@ -60,118 +64,212 @@ export default function RegisterUserpage({ }: Props) {
 
   };
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.image}>
-        <img src="/Logo.png" alt="Logo" />
-      </div>
-      <div className={styles.box}>
-        <div className={styles.header}>
-          <h1>Register for user</h1>
+    <div className={styles.rootContainer}>
+
+      <RootLayout showNavbar={false}>
+        <div className="flex justify-center mx-auto">
+          <div className="text-center mt-5">
+            <Image src={logo} className="img-fluid" width="250" height="150" alt="" />
+          </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>UserName</h2>
-              <input type="text" name="username" value={formData.username} onChange={handleChange} required />
-            </div>
-            <div className={styles.inputGroup}>
-              <h2>FirstName </h2>
-              <input type="text" name="first_name" value={formData.first_name} onChange={handleChange} required />
-            </div>
+
+        <div className="text-center">
+          <div className={`p-4 m-10 rounded-2xl ${styles.borderBox}`}>
+            <span style={{ fontFamily: 'Comfortaa' }} className="text-xl mt-4 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-black font-bold">
+              Register User
+            </span>
+            <form onSubmit={handleSubmit} className="flex flex-col lg:grid lg:grid-cols-2 gap-2 text-center items-center">
+              <div className="mt-10 username">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 firstname">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">First Name</label>
+                <input
+                  type="text"
+                  name="first_name"
+                  value={formData.first_name}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 lastname">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Last Name</label>
+                <input
+                  type="text"
+                  name="last_name"
+                  value={formData.last_name}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 emails">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 passwords">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 districts">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">District</label>
+                <input
+                  type="text"
+                  name="district"
+                  value={formData.district}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 subDistricts">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Sub District</label>
+                <input
+                  type="text"
+                  name="sub_district"
+                  value={formData.sub_district}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 streetnums">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Street Number</label>
+                <input
+                  type="text"
+                  name="street_number"
+                  value={formData.street_number}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 provice">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Province</label>
+                <input
+                  type="text"
+                  name="province"
+                  value={formData.province}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 zipcode">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Zip Code</label>
+                <input
+                  type="text"
+                  name="zip_code"
+                  value={formData.zip_code}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 contactNum">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Contact Number</label>
+                <input
+                  type="text"
+                  name="contact_number"
+                  value={formData.contact_number}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 age">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Age</label>
+                <input
+                  type="number"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  required
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+
+
+              <div className="mt-10 gender">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Gender</label>
+                <select name='gender' value={formData.gender} onChange={handleChange} required>
+                  <option value="">Select Gender</option>
+                  <option value="female">Female</option>
+                  <option value="Male">Male</option>
+                  <option value="PreferNotToSay">Prefer not to say</option>
+                </select>
+              </div>
+
+              <div className="mt-10 locationAll">
+                <label style={{ fontFamily: 'Comfortaa' }} className="block text-start text-black mb-2 text-sm sm:text-sm md:text-sm lg:text-sm xl:text-sm 2xl:text-sm font-semibold">Location All</label>
+                <input
+                  type="text"
+                  name="location_all"
+                  value={formData.locationall}
+                  onChange={handleChange}
+                  required
+                  readOnly
+                  className={`p-2 rounded-3xl`}
+                  style={{ fontFamily: 'Comfortaa' }}
+                />
+              </div>
+
+              <div className="mt-10 col-span-2 text-white bg-red-300 p-4 rounded-2xl">
+                <button style={{ fontFamily: 'Comfortaa' }} type="submit">Register</button>
+              </div>
+            </form>
           </div>
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>LastName</h2>
-              <input type="text" name="last_name" value={formData.last_name} onChange={handleChange} required />
-            </div>
-            <div className={styles.inputGroup}>
-              <h2>Email</h2>
-              <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-            </div>
-          </div>
+        </div>
 
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>Password</h2>
-              <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+      </RootLayout>
 
-            </div>
-
-
-            <div className={styles.inputGroup}>
-              <h2>District</h2>
-              <input type="text" name="district" value={formData.district} onChange={handleChange} required />
-            </div>
-
-
-          </div>
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>Subdistrict</h2>
-              <input type="text" name="sub_district" value={formData.sub_district} onChange={handleChange} required />
-            </div>
-            <div className={styles.inputGroup}>
-              <h2>Street Number</h2>
-              <input type="text" name="street_number" value={formData.street_number} onChange={handleChange} required />
-            </div>
-          </div>
-
-
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>Province</h2>
-              <input type="text" name="province" value={formData.province} onChange={handleChange} required />
-            </div>
-            <div className={styles.inputGroup}>
-              <h2>Zip code</h2>
-              <input type="text" name="zip_code" value={formData.zip_code} onChange={handleChange} required />
-            </div>
-          </div>
-
-
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>Contact Number</h2>
-              <input type="text" name="contact_number" value={formData.contact_number} onChange={handleChange} required />
-            </div>
-            <div className={styles.inputGroup}>
-              <h2>Age</h2>
-              <input type="number" name="age" value={formData.age} onChange={handleChange} required />
-            </div>
-          </div>
-
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>Gender</h2>
-              <select name="gender" value={formData.gender} onChange={handleChange} required>
-                <option value="">Select gender</option>
-                <option value="female">Female</option>
-                <option value="male">Male</option>
-              </select>
-            </div>
-          </div>
-
-          <div className={styles.inputRow}>
-            <div className={styles.inputGroup}>
-              <h2>Location</h2>
-              <input
-                type="text"
-                name="locationall"
-                value={formData.locationall}
-                readOnly // Make the field read-only
-                required
-              />
-            </div>
-          </div>
-
-          <div className={styles.LoginBox}>
-            <div className={styles.login}>
-              <button type="submit" >Register</button>
-            </div>
-          </div>
-
-        </form>
-      </div>
     </div>
   );
 
